5f60b469773b505762000ff3a2dae345e14a0762
Banky <kingmorty@protonmail.com>
2022-12-17 14:34:55 +0100

    Add button on each book to remove it from library

--- scripts/index.js
+++ scripts/index.js
@@ -32,13 +32,28 @@ function displayBooks() {
   const bookList = document.querySelector(".books-container ul");
   bookList.replaceChildren();
 
+  // Loops through each 'book' object in the array and appends them to the HTML, as well as adding an on-click event listener for the 'remove' button
   for (const book of myLibrary) {
     const BookItem = document.createElement("li");
+    BookItem.setAttribute("data-library-index", myLibrary.indexOf(book));
 
+    // Button for removing books
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.className = "remove-book";
+    removeBtn.addEventListener("click", () => {
+      if (confirm("Are you certain you want to remove this book?")) {
+        myLibrary.splice(BookItem.getAttribute("data-library-index"), 1);
+        displayBooks();
+      }
+    });
+
+    // Elements to display 'book' content
     const BookTitle = document.createElement("p");
     const BookAuthor = document.createElement("p");
     const BookPages = document.createElement("p");
     const BookReadStatus = document.createElement("p");
+
     BookTitle.textContent = book.title;
     BookAuthor.textContent = book.author;
     BookPages.textContent = book.numOfPages;
@@ -48,12 +63,20 @@ function displayBooks() {
     BookItem.appendChild(BookAuthor);
     BookItem.appendChild(BookPages);
     BookItem.appendChild(BookReadStatus);
+    BookItem.appendChild(removeBtn);
 
+    // Finally append the new book item to the list of books in the html
     bookList.appendChild(BookItem);
   }
 }
 
-// Hide and display form
+// Store and update user data by creating new book objects and storing them in an array
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  displayBooks();
+}
+
 const newBookContainer = document.querySelector(".new-book-container");
 const newBookForm = document.querySelector(".new-book-container form");
 const newBookBtn = document.getElementById("new-book-btn");
@@ -61,23 +84,18 @@ const newBookCancelBtn = document.getElementById("cancel-btn");
 const newBookSubmitBtn = document.getElementById("submit-btn");
 newBookContainer.removeChild(newBookForm);
 
+// Display form and remove buton on click
 newBookBtn.addEventListener("click", () => {
   newBookContainer.appendChild(newBookForm);
   newBookContainer.removeChild(newBookBtn);
 });
 
+// Remove form and display button on click
 newBookCancelBtn.addEventListener("click", (e) => {
   e.preventDefault();
   newBookContainer.removeChild(newBookForm);
   newBookContainer.appendChild(newBookBtn);
 });
-
-// Store and update user data
-function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-  displayBooks();
-}
 
 // Collect and process user data
 newBookSubmitBtn.addEventListener("click", (e) => {
@@ -85,7 +103,7 @@ newBookSubmitBtn.addEventListener("click", (e) => {
   const titleInput = document.getElementById("title");
   const authorInput = document.getElementById("author");
   const pagesInput = document.getElementById("pages");
-  const readInput = document.getElementById("read-status");
+  const readStatusInput = document.getElementById("read-status");
 
   if (
     titleInput.value === "" ||
@@ -95,17 +113,24 @@ newBookSubmitBtn.addEventListener("click", (e) => {
     return;
   }
 
+  let readStatus;
+  if (readStatusInput.checked === true) {
+    readStatus = "Has been read";
+  } else {
+    readStatus = "Has not been read";
+  }
+
   addBookToLibrary(
     titleInput.value,
     authorInput.value,
     pagesInput.value,
-    readInput.checked
+    readStatus
   );
 
   titleInput.value = "";
   authorInput.value = "";
   pagesInput.value = "";
-  readInput.checked = false;
+  readStatusInput.checked = false;
 });
 
-displayBooks();
+displayBooks(); // Updates pre-saved content unto the html
